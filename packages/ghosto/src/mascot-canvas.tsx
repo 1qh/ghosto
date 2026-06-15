145bab0d70637856a168f2a3c12b4a2880fc3034
Lai Quang Huy <laiquanghuy24122001@gmail.com>
2026-06-16 05:15:44 +0700

    chore(lint): drop react-compiler suppressions now allowed globally in lintmax 0.1.69

--- packages/ghosto/src/mascot-canvas.tsx
+++ packages/ghosto/src/mascot-canvas.tsx
@@ -1,5 +1,5 @@
 /** biome-ignore-all lint/performance/noNamespaceImport: three convention */
-/* oxlint-disable import/no-namespace, react/react-compiler */
+/* oxlint-disable import/no-namespace */
 /* eslint-disable react/no-unknown-property, @eslint-react/dom/no-unknown-property, react-hooks/refs, react-hooks/immutability */
 'use client'
 import { Canvas, useThree } from '@react-three/fiber'
